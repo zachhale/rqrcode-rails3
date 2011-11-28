cb7035ee808270b1b61e2e65c8f1cfaad1b6f151
Zach Hale <zachhale@gmail.com>
2011-11-27 23:10:50 -0800

    Refacotred SVG body into method so it can be extended

--- lib/rqrcode-rails3/renderers/svg.rb
+++ lib/rqrcode-rails3/renderers/svg.rb
@@ -9,21 +9,28 @@ module RQRCode
         open_tag  = %{<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" width="#{dimension}" height="#{dimension}">}
         close_tag = "</svg>"
 
-        result = []
+        body = render_body
+
+        svg = [xml_tag, open_tag, body, close_tag].flatten.join("\n")
+      end
+
+      def render_body
+        body = []
+
         qrcode.modules.each_index do |y_module|
           tmp = []
           qrcode.modules.each_index do |x_module|
             cell = render_cell(y_module, x_module)
             tmp << cell if cell
           end
-          result << tmp.join
+          body << tmp.join
         end
 
         if fill
-          result.unshift %{<rect width="#{dimension}" height="#{dimension}" x="0" y="0" style="fill:##{fill}"/>}
+          body.unshift %{<rect width="#{dimension}" height="#{dimension}" x="0" y="0" style="fill:##{fill}"/>}
         end
 
-        svg = [xml_tag, open_tag, result, close_tag].flatten.join("\n")
+        body
       end
 
       def render_cell(y_module, x_module)
